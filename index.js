430cfb8108abdb5d1b16aa1c74059d08aab12b5e
ERROR\Name <baconcheese113@hotmail.com>
2019-06-10 19:41:44 -0600

    - Converted all jpg's to webp - Gallery styling changed for desktop - Added blog page for listing all posts

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+app.get('/gallery', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'gallery.html'))
+})
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'))
+})
+app.get('/blog', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'blog.html'))
+})
 
 app.listen(process.env.PORT || PORT) 
